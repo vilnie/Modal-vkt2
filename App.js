2e943ecda25fa7d450af613de6541723ee8b4064
Ville Niemi <vipeni35@gmail.com>
2024-01-19 15:29:48 +0200

    jou

--- App.js
+++ App.js
@@ -1,12 +1,38 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, Modal, Pressable } from 'react-native';
 
 export default function App() {
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+
+      <MyModal visible={showModal} onClose={() => setShowModal(false)} />
+
+      <Pressable onPress={() => setShowModal(true)}>
+        <Text>Press me, please</Text>
+      </Pressable>
     </View>
+  );
+}
+
+function MyModal({ visible, onClose }) {
+  return (
+    <Modal
+      transparent={true}
+      visible={visible}
+      onRequestClose={onClose}
+    >
+      <View style={styles.modalContainer}>
+        <View style={styles.modalContent}>
+          <Text>This is the modal...</Text>
+          <Button title="Close" onPress={onClose} />
+        </View>
+      </View>
+    </Modal>
   );
 }
 
@@ -17,4 +43,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContent: {
+    backgroundColor: '#ffff',
+    padding: 20,
+    borderRadius: 5,
+    alignItems: 'center',
+  },
 });
+
